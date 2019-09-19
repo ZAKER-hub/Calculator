82b37675bc8e6a2215defab2dc434abfbcc2fa3b
333zloykot666 <333zloykot666@gmail.com>
2019-09-20 01:30:33 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.delit = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.umnoz = new System.Windows.Forms.Button();
+            this.rovno = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(14, 266);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(79, 66);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(14, 436);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(79, 66);
+            this.b7.TabIndex = 1;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(194, 351);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(79, 66);
+            this.b6.TabIndex = 2;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(104, 351);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(79, 66);
+            this.b5.TabIndex = 3;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(14, 351);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(79, 66);
+            this.b4.TabIndex = 4;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(194, 266);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(79, 66);
+            this.b3.TabIndex = 5;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(104, 266);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(79, 66);
+            this.b2.TabIndex = 6;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(194, 436);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(79, 66);
+            this.b9.TabIndex = 7;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(104, 436);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(79, 66);
+            this.b8.TabIndex = 8;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // b0
+            // 
+            this.b0.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b0.Location = new System.Drawing.Point(14, 513);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(169, 69);
+            this.b0.TabIndex = 9;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 48);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // delit
+            // 
+            this.delit.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delit.Location = new System.Drawing.Point(279, 266);
+            this.delit.Name = "delit";
+            this.delit.Size = new System.Drawing.Size(64, 66);
+            this.delit.TabIndex = 11;
+            this.delit.Text = "/";
+            this.delit.UseVisualStyleBackColor = true;
+            this.delit.Click += new System.EventHandler(this.Delit_Click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.Location = new System.Drawing.Point(279, 513);
+            this.plus.Name = "plus";
+            this.plus.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.plus.Size = new System.Drawing.Size(64, 66);
+            this.plus.TabIndex = 12;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(279, 436);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(64, 66);
+            this.minus.TabIndex = 13;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // umnoz
+            // 
+            this.umnoz.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.umnoz.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.umnoz.Location = new System.Drawing.Point(279, 351);
+            this.umnoz.Margin = new System.Windows.Forms.Padding(10);
+            this.umnoz.Name = "umnoz";
+            this.umnoz.Padding = new System.Windows.Forms.Padding(0, 11, 0, 0);
+            this.umnoz.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.umnoz.Size = new System.Drawing.Size(64, 66);
+            this.umnoz.TabIndex = 14;
+            this.umnoz.Text = "*";
+            this.umnoz.UseVisualStyleBackColor = true;
+            this.umnoz.Click += new System.EventHandler(this.Umnoz_Click);
+            // 
+            // rovno
+            // 
+            this.rovno.Font = new System.Drawing.Font("Unispace", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rovno.Location = new System.Drawing.Point(194, 514);
+            this.rovno.Name = "rovno";
+            this.rovno.Size = new System.Drawing.Size(79, 66);
+            this.rovno.TabIndex = 15;
+            this.rovno.Text = "=";
+            this.rovno.UseVisualStyleBackColor = true;
+            this.rovno.Click += new System.EventHandler(this.Rovno_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(-1, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 51);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Unispace", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 66);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 594);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rovno);
+            this.Controls.Add(this.umnoz);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.delit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button delit;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button umnoz;
+        private System.Windows.Forms.Button rovno;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public static string a = " ";
+		
+		public List<string> list =new List<string>();
+
+		public int sum = 0;
+		void unit()
+		{
+			for (int i = 0; i < list.Count(); i++) {
+				try
+				{
+					if (!(list[i + 1] == "+") & !(list[i] == "+")
+						& !(list[i + 1] == "-") & !(list[i] == "-")
+						& !(list[i + 1] == "*") & !(list[i] == "*")
+						& !(list[i + 1] == "/") & !(list[i] == "/"))
+					{
+
+						list[i] = list[i] + list[i + 1];
+						list.RemoveAt(i + 1);
+						unit();
+					}
+				}
+				catch (ArgumentOutOfRangeException) { }
+
+			}
+		}
+
+		void operation()
+		{
+			int i = 0;
+
+			 while (i < list.Count()) {
+				if (list.Contains("*"))
+				{
+					i = list.IndexOf("*");
+				}
+				try
+				{
+					switch (list[i])
+					{
+						case "+":
+							sum = Convert.ToInt32(list[i - 1]) + Convert.ToInt32((list[i + 1]));
+							list[i] = sum.ToString();
+							list.RemoveAt(i + 1);
+							list.RemoveAt(i - 1);
+							break;
+						case "-":
+							sum = Convert.ToInt32(list[i - 1]) - Convert.ToInt32((list[i + 1]));
+							list[i] = sum.ToString();
+							list.RemoveAt(i + 1);
+							list.RemoveAt(i - 1);
+							break;
+						case "*":
+							sum = Convert.ToInt32(list[i - 1]) * Convert.ToInt32((list[i + 1]));
+							list[i] = sum.ToString();
+							list.RemoveAt(i + 1);
+							list.RemoveAt(i - 1);
+							i = 0;
+							break;
+						case "/":
+							sum = Convert.ToInt32(list[i - 1]) / Convert.ToInt32((list[i + 1]));
+							list[i] = sum.ToString();
+							list.RemoveAt(i + 1);
+							list.RemoveAt(i - 1);
+							break;
+
+					}
+				}
+				catch (ArgumentOutOfRangeException) { }
+				i += 1;
+			 } 
+		}
+		void FOR()
+		{
+			for (int i = 0; i < list.Count(); i++)
+			{
+				Console.WriteLine(list[i]);
+			}
+			Console.WriteLine(sum);
+		}
+	
+		public void label() {
+			unit();
+			if (a.Contains("="))
+			{
+				operation();
+			}
+			FOR();
+			label1.Text = a;
+			label2.Text = sum.ToString();
+			
+			if (a.Length > 12) {
+				label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+
+			}
+
+		}
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			a += '1';
+			list.Add("1");
+			label();
+		}
+
+		private void Button2_Click(object sender, EventArgs e)
+		{
+			a += '2';
+			list.Add("2");
+			label();
+		}
+
+		private void Button3_Click(object sender, EventArgs e)
+		{
+			a += '3';
+			list.Add("3");
+			label();
+		}
+
+		private void Button4_Click(object sender, EventArgs e)
+		{
+			a += '4';
+			list.Add("4");
+			label();
+		}
+
+		private void Button5_Click(object sender, EventArgs e)
+		{
+			a += '5';
+			list.Add("5");
+			label();
+		}
+
+		private void Button6_Click(object sender, EventArgs e)
+		{
+			a += '6';
+			list.Add("6");
+			label();
+		}
+
+		private void Button7_Click(object sender, EventArgs e)
+		{
+			a += '7';
+			list.Add("7");
+			label();
+		}
+
+		private void Button8_Click(object sender, EventArgs e)
+		{
+			a += '8';
+			list.Add("8");
+			label();
+		}
+
+		private void Button9_Click(object sender, EventArgs e)
+		{
+			a += '9';
+			list.Add("9");
+			label();
+		}
+
+		private void Button10_Click(object sender, EventArgs e)
+		{
+			a += '0';
+			list.Add("0");
+			label();
+		}
+
+		private void Delit_Click(object sender, EventArgs e)
+		{
+			a += '/';
+			list.Add("/");
+			label();
+		}
+
+		private void Umnoz_Click(object sender, EventArgs e)
+		{
+			a += '*';
+			list.Add("*");
+			label();
+		}
+
+		private void Minus_Click(object sender, EventArgs e)
+		{
+			a += '-';
+			list.Add("-");
+			label();
+		}
+
+		private void Plus_Click(object sender, EventArgs e)
+		{
+			a += '+';
+			list.Add("+");
+			label();
+		}
+
+		private void Rovno_Click(object sender, EventArgs e)
+		{
+			a += '=';
+			label();
+		}
+
+		private void Button1_Click_1(object sender, EventArgs e)
+		{
+			list.Clear();
+			a = " ";
+			label1.Text = "0";
+			label2.Text = "0";
+			label();
+		}
+	}
+}
